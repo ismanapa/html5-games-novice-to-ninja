245436f05e51608e3d2dccca8ed4898bc01327db
Ismael Navarro <inavarro@plainconcepts.com>
2020-06-17 08:55:34 +0200

    Added random waypoints to bats

--- src/chapter05/entities/BatBehaviour.ts
+++ src/chapter05/entities/BatBehaviour.ts
@@ -4,14 +4,27 @@ import { Bat } from './Bat';
 
 export class BatBehaviour implements UpdateBehaviour {
   update(dt: number, t: number, entity: Bat): void {
-    const { pos, dir, speed } = entity;
+    const { pos, speed, waypoint } = entity;
 
-    const { x, y } = dir;
-    const xo = x * dt * speed;
-    const yo = y * dt * speed;
-    pos.x += xo;
-    pos.y += yo;
-    pos.y += Math.sin((t + speed) * 10) * speed * dt;
+    // Move in the direction of the path
+    const xo = waypoint.x - pos.x + 4;
+    const yo = waypoint.y - pos.y - 1;
+
+    const step = speed * dt;
+    const xIsClose = Math.abs(xo) <= step;
+    const yIsClose = Math.abs(yo) <= step;
+
+    if (!xIsClose) {
+      pos.x += speed * (xo > 0 ? 1 : -1) * dt;
+    }
+    if (!yIsClose) {
+      pos.y += speed * (yo > 0 ? 1 : -1) * dt;
+    }
+
+    if (xIsClose && yIsClose) {
+      // New way point
+      entity.waypoint = entity.findWaypoint();
+    }
 
     entity.frame.x = ((t / 0.1) | 0) % 2 + 3;
   }
